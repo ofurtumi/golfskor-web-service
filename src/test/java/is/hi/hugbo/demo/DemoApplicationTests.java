6d5a17e2a62d7ac70bc83aaeef43e8ad14ef50d4
Þorvaldur Tumi <tumi@juni.is>
2023-09-02 13:43:58 +0000

    create skeleton

---
+++ src/test/java/is/hi/hugbo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package is.hi.hugbo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
