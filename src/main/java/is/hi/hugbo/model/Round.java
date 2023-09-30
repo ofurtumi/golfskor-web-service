53dda2c71d04f7896e3bf35571facf415cff30d3
Þorvaldur Tumi <tumi@juni.is>
2023-09-30 15:06:59 +0000

    add two new models holes and round

--- src/main/java/is/hi/hugbo/model/Round.java
+++ src/main/java/is/hi/hugbo/model/Round.java
@@ -20,6 +20,10 @@ public class Round {
   public Round() {
   }
 
+  public Round(int hole1, int hole2, int hole3, int hole4, int hole5, int hole6, int hole7, int hole8, int hole9) {
+    this.holes = new int[] { hole1, hole2, hole3, hole4, hole5, hole6, hole7, hole8, hole9 };
+  }
+
   public Round(Course course, User user, int[] holes) {
     this.course = course;
     this.user = user;
