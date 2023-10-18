b151cd282ba346fd7231d496fda9f7b43675f79b
hakoningir <hir12@hi.is>
2023-10-18 21:26:42 +0000

    feat/show hide functions for picking how many holes to play

--- src/main/java/is/hi/hugbo/model/Holes.java
+++ src/main/java/is/hi/hugbo/model/Holes.java
@@ -10,11 +10,21 @@ public class Holes {
   private int hole7;
   private int hole8;
   private int hole9;
+  private int hole10;
+  private int hole11;
+  private int hole12;
+  private int hole13;
+  private int hole14;
+  private int hole15;
+  private int hole16;
+  private int hole17;
+  private int hole18;
 
   public Holes() {
   }
 
-  public Holes(int hole1, int hole2, int hole3, int hole4, int hole5, int hole6, int hole7, int hole8, int hole9) {
+  public Holes(int hole1, int hole2, int hole3, int hole4, int hole5, int hole6, int hole7, int hole8, int hole9, 
+  int hole10, int hole11, int hole12, int hole13, int hole14, int hole15, int hole16, int hole17, int hole18) {
     this.hole1 = hole1;
     this.hole2 = hole2;
     this.hole3 = hole3;
@@ -24,10 +34,20 @@ public class Holes {
     this.hole7 = hole7;
     this.hole8 = hole8;
     this.hole9 = hole9;
+    this.hole10 = hole10;
+    this.hole11 = hole11;
+    this.hole12 = hole12;
+    this.hole13 = hole13;
+    this.hole14 = hole14;
+    this.hole15 = hole15;
+    this.hole16 = hole16;
+    this.hole17 = hole17;
+    this.hole18 = hole18;
   }
 
   public int[] getHoles() {
-    int[] holes = { hole1, hole2, hole3, hole4, hole5, hole6, hole7, hole8, hole9 };
+    int[] holes = { hole1, hole2, hole3, hole4, hole5, hole6, hole7, hole8, hole9, hole10, 
+                    hole11, hole12, hole13, hole14, hole15, hole16, hole17, hole18 };
     return holes;
   }
 
@@ -101,5 +121,77 @@ public class Holes {
 
   public void setHole9(int hole9) {
     this.hole9 = hole9;
+  }
+
+  public int getHole10() {
+    return hole10;
+  }
+
+  public void setHole10(int hole10) {
+    this.hole10 = hole10;
+  }
+
+  public int getHole11() {
+    return hole11;
+  }
+
+  public void setHole11(int hole11) {
+    this.hole11 = hole11;
+  }
+  
+  public int getHole12() {
+    return hole12;
+  }
+
+  public void setHole12(int hole12) {
+    this.hole12 = hole12;
+  }
+  
+  public int getHole13() {
+    return hole13;
+  }
+
+  public void setHole13(int hole13) {
+    this.hole13 = hole13;
+  }
+  
+  public int getHole14() {
+    return hole14;
+  }
+
+  public void setHole14(int hole14) {
+    this.hole14 = hole14;
+  }
+  
+  public int getHole15() {
+    return hole15;
+  }
+
+  public void setHole15(int hole15) {
+    this.hole15 = hole15;
+  }
+
+  public int getHole16() {
+    return hole16;
+  }
+
+  public void setHole16(int hole16) {
+    this.hole16 = hole16;
+  }
+
+  public int getHole17() {
+    return hole17;
+  }
+
+  public void setHole17(int hole17) {
+    this.hole17 = hole17;
+  }
+
+  public int getHole18() {
+    return hole18;
+  }
+
+  public void setHole18(int hole18) {
+    this.hole18 = hole18;
   }
 }
