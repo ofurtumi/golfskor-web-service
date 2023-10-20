2242edeec7d1d94fe0f1bc55105522fb6066be81
hakoningir <hir12@hi.is>
2023-10-20 12:18:41 +0000

    9 hole logging is ready

--- src/main/java/is/hi/hugbo/model/Holes.java
+++ src/main/java/is/hi/hugbo/model/Holes.java
@@ -46,6 +46,10 @@ public class Holes {
   }
 
   public int[] getHoles() {
+    if(hole10 == 0){
+      int[] holes = { hole1, hole2, hole3, hole4, hole5, hole6, hole7, hole8, hole9 };
+      return holes;
+    }
     int[] holes = { hole1, hole2, hole3, hole4, hole5, hole6, hole7, hole8, hole9, hole10, 
                     hole11, hole12, hole13, hole14, hole15, hole16, hole17, hole18 };
     return holes;
